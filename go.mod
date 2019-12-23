54e7c7414021cc5d385992d9f85b8fee7a89571b
amiralii <mr.roshanae@gmail.com>
2019-12-23 06:48:50 +0330

    Add gomail

--- go.mod
+++ go.mod
@@ -14,6 +14,8 @@ require (
 	golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2
 	golang.org/x/sys v0.0.0-20191220220014-0732a990476f // indirect
 	google.golang.org/appengine v1.6.5 // indirect
+	gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
+	gopkg.in/gomail.v2 v2.0.0-20160411212932-81ebce5c23df
 	gopkg.in/tucnak/telebot.v2 v2.0.0-20191005061224-d0707a9d73c4
 	gopkg.in/yaml.v2 v2.2.7 // indirect
 )
--- helpers/email.go
+++ helpers/email.go
@@ -1,20 +1,26 @@
 package helpers
 
 import (
+	"crypto/tls"
 	"github.com/spf13/viper"
+	"gopkg.in/gomail.v2"
 	"log"
-	"net/smtp"
 )
 
 func SendEmail(body, to string) {
 	from := viper.GetString("EMAIL.FROM")
 	pass := viper.GetString("EMAIL.PASSWORD")
-	msg := []byte("From: " + from + "\n" + "To: " + to + "\n" + "Subject: Joining to Channel/Group\n\n" + body)
-	auth := smtp.PlainAuth("", from, pass, viper.GetString("EMAIL.PROVIDER"))
-	serverAddress := viper.GetString("EMAIL.PROVIDER") + ":" + viper.GetString("EMAIL.PORT")
-	receiver := []string{to}
-	err := smtp.SendMail(serverAddress, auth, from, receiver, msg)
-	if err != nil {
+	userName := viper.GetString("EMAIL.USERNAME")
+	serverAddress := viper.GetString("EMAIL.PROVIDER")
+	serverPort := viper.GetInt("EMAIL.PORT")
+	m := gomail.NewMessage()
+	m.SetHeader("From", from)
+	m.SetHeader("To", to)
+	m.SetHeader("Subject", "Candor Bot Activation Key")
+	m.SetBody("text/html", "Your activation key is:   <b>"+body+"</b> this key will be expire about 10 minutes")
+	d := gomail.NewDialer(serverAddress, serverPort, userName, pass)
+	d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+	if err := d.DialAndSend(m); err != nil {
 		log.Printf("smtp error: %s", err)
 		return
 	}
