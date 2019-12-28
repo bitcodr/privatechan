8dd1f261d9e4d9d24fa12a983aac4b5eed87dba3
amiralii <mr.roshanae@gmail.com>
2019-12-28 11:51:02 +0330

    fixed

--- helpers/reflection.go
+++ helpers/reflection.go
@@ -2,13 +2,11 @@ package helpers
 
 import "reflect"
 
-
 //Invoke function
 func Invoke(any interface{}, result *bool, name string, args ...interface{}) {
 	inputs := make([]reflect.Value, len(args))
 	for i := 0; i < len(args); i++ {
 		inputs[i] = reflect.ValueOf(args[i])
 	}
-	reflect.ValueOf(any).MethodByName(name).Call(inputs)
-	*result = true
+	*result = reflect.ValueOf(any).MethodByName(name).Call(inputs)[0].Bool()
 }
