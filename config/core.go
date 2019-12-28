5c6deac89a3024a48f155a8e731ad4814408e913
amiralii <mr.roshanae@gmail.com>
2019-12-28 23:05:01 +0330

    fixed

--- config/core.go
+++ config/core.go
@@ -18,13 +18,12 @@ type App struct {
 	ProjectDir, BotToken, BotUsername, DBName, DBUserName, DBPass, CurrentTime string
 }
 
-
-func (app *App) SetAppConfig() *App{
+func (app *App) SetAppConfig() *App {
 	app.BotToken = viper.GetString("APP.TELEGRAM_API_TOKEN")
 	app.DBName = viper.GetString("DATABASES.MYSQL.DATABASE")
 	app.DBUserName = viper.GetString("DATABASES.MYSQL.USERNAME")
 	app.DBPass = viper.GetString("DATABASES.MYSQL.PASSWORD")
-	app.CurrentTime= time.Now().UTC().Format("2006-01-02 03:04:05")
-	app.BotUsername = viper.GetString("APP.BOT_USERNAME")
+	app.CurrentTime = time.Now().UTC().Format("2006-01-02 03:04:05")
+	app.BotUsername = viper.GetString("APP.APP.BOT_USERNAME")
 	return app
 }
