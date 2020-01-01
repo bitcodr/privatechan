5599da2dfb492db3608719608d3100bc2b729473
amiralii <mr.roshanae@gmail.com>
2020-01-01 09:02:15 +0330

    fixed

--- config/database.go
+++ config/database.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
+	"time"
 )
 
 func (app *App) DB() *sql.DB {
@@ -12,8 +13,8 @@ func (app *App) DB() *sql.DB {
 	if err != nil {
 		log.Fatalln(err)
 	}
-	// db.SetConnMaxLifetime(time.Second * 5)
-	// db.SetMaxIdleConns(0)
-	// db.SetMaxOpenConns(151)
+	db.SetConnMaxLifetime(time.Second * 5)
+	db.SetMaxIdleConns(0)
+	db.SetMaxOpenConns(151)
 	return db
 }
--- config/environment.go
+++ config/environment.go
@@ -8,7 +8,7 @@ import (
 func (app *App) Environment() {
 	viper.SetConfigName("config")
 	viper.AddConfigPath(app.ProjectDir)
-	// viper.AddConfigPath("/var/www/privatechan")
+	viper.AddConfigPath("/var/www/privatechan")
 	err := viper.ReadInConfig()
 	if err != nil {
 		log.Fatalln(err)
