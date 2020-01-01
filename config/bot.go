da64ffced48cb936f087536204b87d88f934e346
amiralii <mr.roshanae@gmail.com>
2020-01-01 09:52:23 +0330

    fixed

--- config/bot.go
+++ config/bot.go
@@ -3,13 +3,24 @@ package config
 import (
 	tb "gopkg.in/tucnak/telebot.v2"
 	"log"
+	"strings"
 	"time"
 )
 
 func (app *App) Bot() *tb.Bot {
+	poller := &tb.LongPoller{Timeout: 15 * time.Second}
+	spamProtected := tb.NewMiddlewarePoller(poller, func(upd *tb.Update) bool {
+		if upd.Message == nil {
+			return true
+		}
+		if strings.Contains(upd.Message.Text, "spam") {
+			return false
+		}
+		return true
+	})
 	bot, err := tb.NewBot(tb.Settings{
 		Token:  app.BotToken,
-		Poller: &tb.LongPoller{Timeout: 10 * time.Second},
+		Poller: spamProtected,
 	})
 	if err != nil {
 		log.Fatalln(err)
