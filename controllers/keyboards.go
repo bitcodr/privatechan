cfb8e8a135a73ec94c91b962e70569106f798fe8
amiralii <mr.roshanae@gmail.com>
2020-01-03 18:04:37 +0330

    fixed

--- controllers/keyboards.go
+++ controllers/keyboards.go
@@ -2,20 +2,19 @@
 package controllers
 
 import (
-	"github.com/amiraliio/tgbp/config"
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
 //TODO needs keyboard refactoring
 //bot startup buttons
 var addAnonMessage = tb.ReplyButton{
-	Text: config.LangConfig.GetString("MESSAGES.ADD_MESSAGE_TO_CHANNEL"),
+	Text: "Add Anonymous Message to a Channel/Group",
 }
 var setupVerifiedCompany = tb.ReplyButton{
-	Text: config.LangConfig.GetString("MESSAGES.SETUP_VERIFIED_COMPANY"),
+	Text: "Setup Verified Company Account",
 }
 var joinCompanyChannels = tb.ReplyButton{
-	Text: config.LangConfig.GetString("MESSAGES.JOIN_TO_COMPANY_CHANNEL"),
+	Text: "Join To Company Anonymous Channel/Group",
 }
 var StartBotKeys = [][]tb.ReplyButton{
 	[]tb.ReplyButton{addAnonMessage},
