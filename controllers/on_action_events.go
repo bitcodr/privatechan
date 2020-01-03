621b26f9b89e0abd6662156ca60e6dd7b2fa2857
amiralii <mr.roshanae@gmail.com>
2020-01-04 02:49:04 +0330

    fixed

--- controllers/on_action_events.go
+++ controllers/on_action_events.go
@@ -1,11 +1,25 @@
 package controllers
 
 import (
+	"fmt"
 	"github.com/amiraliio/tgbp/config"
 	tb "gopkg.in/tucnak/telebot.v2"
+	"log"
 )
 
 func onActionEvents(app *config.App, bot *tb.Bot) {
+
+	bot.Handle(nil, func(message *tb.Message) {
+		if message.FromGroup() {
+			admins, err := bot.AdminsOf(message.Chat)
+			if err != nil {
+				log.Println(err)
+			}
+			fmt.Println(admins)
+			return
+		}
+		fmt.Println("false")
+	})
 
 	bot.Handle(tb.OnChannelPost, func(message *tb.Message) {
 		if generalEventsHandler(app, bot, message, &Event{
